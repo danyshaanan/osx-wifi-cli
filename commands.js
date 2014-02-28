e42bbbfa85f15555bc22790de5d7431306242a65
Dany <danyshaanan@gmail.com>
2014-02-28 18:07:00 +0200

    first working version

---
+++ commands.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+module.exports = {
+  osx: {
+    on: 'networksetup -setairportpower en0 on',
+    off: 'networksetup -setairportpower en0 off',
+    scan: '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan',
+    connect: 'networksetup -setairportnetwork en0 "NETWORK_TOKEN" "PASSWORD_TOKEN"',
+    parseScan: function(output) {
+      var lineRegex = /^\s+(.*)\s+([a-z0-9:]{17})\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+$/;
+      return output.split('\n').map(function(line) {
+        var parts = line.match(lineRegex);
+        return parts && { SSID: parts[1], BSSID: parts[2], RSSI: parseInt(parts[3]), CHANNEL: parseInt(parts[4]), HT: parts[5], CC: parts[6], SECURITY: parts[7] };
+      }).filter(Boolean);
+    },
+    currentNetwork: '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I',
+    extractCurrentNetwork: function(output) {
+      return output.split('\n').reduce(function(obj, pair) {
+          var match = pair.match(/^\s+([^:]+):\s+(.*)(\b\s+)?$/);
+          if (match) obj[match[1]] = isNaN(match[2]) ? match[2] : parseInt(match[2]);
+          return obj;
+      }, {});
+    }
+  }
+}
---
+++ osx-wifi-cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var Q = require('q');
+var cli = require('commander');
+var exec = require('child_process').exec;
+var rek = require('rekuire');
+var commands = rek('commands');
+var version = rek('package.json').version;
+
+cli
+  .version(version)
+  .usage('[on | off | restart | scan | <network> <password>]')
+  .option('on', 'turn wifi on')
+  .option('off', 'turn wifi off')
+  .option('restart', 'restart wifi')
+  .option('scan', 'show available networks')
+  .parse(process.argv);
+
+var utils = commands.osx; // If implementing other OSs, this is the place to check which we're on.
+
+var args = process.argv.slice(2);
+
+if      (args[0] == 'on')   execute(utils.on); //cli.on is a function
+else if (cli.off)           execute(utils.off);
+else if (cli.restart)       execute(utils.off).then(execute.bind(this,utils.on));
+else if (cli.scan)          execute(utils.scan).then(utils.parseScan).then(printNetworks);
+else if (args.length == 2)  execute(utils.connect.replace('NETWORK_TOKEN',args[0]).replace('PASSWORD_TOKEN',args[1]))
+else if (args.length == 0)  execute(utils.currentNetwork).then(utils.extractCurrentNetwork).then(help);
+else                        console.log('input error')
+
+////////////////////////////////////////////////
+
+function help(currentNetwork) {
+  if (currentNetwork.SSID) {
+    console.log('you are connected to ' + currentNetwork.SSID);
+  } else {
+    console.log('you are not connected anywhere');
+  }
+  //TODO: add more help text
+}
+
+function printNetworks(networksObj) {
+  //TODO: Add padding, numeric IDs, and colors
+  var output = networksObj.map(function(network) { return Object.keys(network).map(function(key) {return network[key];}).join(' ')}).join('\n');
+  console.log(output);
+}
+
+function execute(cmd) {
+  //console.log('executing command:', cmd);
+  var deferred = Q.defer();
+  exec(cmd, function(err, strout, strerr) {
+    if (err) deferred.reject(new Error(err));
+    else if (strerr) deferred.reject(new Error(strerr));
+    else deferred.resolve(strout);
+  });
+  return deferred.promise;
+};
+
+
+
