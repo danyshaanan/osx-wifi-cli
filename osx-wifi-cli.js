4e3bda8bd7e2fe6c7d86d73867653723f2330dc8
Dany Shaanan <danyshaanan@gmail.com>
2017-06-13 14:14:48 +0300

    Add "osx-wifi-cli pass" command

--- osx-wifi-cli.js
+++ osx-wifi-cli.js
@@ -9,11 +9,12 @@ var version = require('./package.json').version
 
 cli
   .version(version)
-  .usage('[on | off | restart | scan | <network> <password>]')
+  .usage('[on | off | restart | scan | pass | <network> <password>]')
   .option('on', 'turn wifi on')
   .option('off', 'turn wifi off')
   .option('restart', 'restart wifi')
   .option('scan', 'show available networks')
+  .option('pass', 'show password for current network')
   .option('--device <device>', 'set device (default is en0)') // see ugly todo below.
   .parse(process.argv)
 
@@ -22,6 +23,7 @@ var commands = {
     on: 'networksetup -setairportpower en0 on',
     off: 'networksetup -setairportpower en0 off',
     scan: '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan',
+    pass: 'security find-generic-password -wa "PASS"',
     connect: 'networksetup -setairportnetwork en0 "NETWORK_TOKEN" "PASSWORD_TOKEN"',
     currentNetwork: '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep -e "\\bSSID:" | sed -e "s/^.*SSID: //"'
   }
@@ -49,6 +51,8 @@ if (args[0] === 'on') {
   execute(utils.off).then(execute.bind(this, utils.on))
 } else if (cli.scan) {
   execute(utils.scan).then(console.log.bind(console))
+} else if (cli.pass) {
+  execute(utils.currentNetwork).then(getPass).then(logTrim)
 } else if (args.length === 2) {
   execute(utils.connect.replace('NETWORK_TOKEN', args[0]).replace('PASSWORD_TOKEN', args[1]))
 } else if (args.length === 0) {
@@ -62,6 +66,14 @@ if (args[0] === 'on') {
 function help(SSID) {
   console.log(SSID.trim() ? 'you are connected to ' + SSID : 'you are not connected anywhere')
   // TODO: add more help text
+}
+
+function getPass(SSID) {
+  return execute(utils.pass.replace('PASS', SSID.trim()))
+}
+
+function logTrim(str) {
+  console.log(str.trim())
 }
 
 function execute(cmd) {
